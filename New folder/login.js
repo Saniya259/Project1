de7daf1ae20f09f45f2704fdff273e39477418ce
Saniya259 <saniyarampur783@gmail.com>
2024-12-13 17:34:47 +0530

    Update login.js

--- New folder/login.js
+++ New folder/login.js
@@ -1,8 +1,5 @@
 document.getElementById("login-form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent the form from submitting the traditional way
+    event.preventDefault(); 
     
-    // You can add validation or authentication logic here if needed
-    
-    // Redirect to index.html after submission
-    window.location.href = "index.html"; // Redirects the user to the index page
+   window.location.href = "index.html"; 
 });
